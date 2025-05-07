1043207b604d06b021a0682ab3a6eac8d2e6126a
2005LIKHITH <likhithcvsrl@gmail.com>
2025-05-07 17:26:50 +0530

    commit

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/LikhithMar14/social/internal/env"
+)
+
+func main(){
+
+	cfg := config{
+		addr: env.GetString("ADDR",":8080"),
+	}
+
+	app := &application{
+		config: cfg,	
+	}
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+	
+
+}
---
+++ internal/env/env.go
@@ -0,0 +1,24 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key,fallback string) string{
+	if value,ok := os.LookupEnv(key); ok{
+		return value
+	}
+	return fallback
+}
+
+func GetInt(key string,fallback int) int{
+	if value,ok := os.LookupEnv(key); ok{
+		parsedValue,err := strconv.Atoi(value)
+		if err != nil{
+			return fallback
+		}
+		return parsedValue
+	}
+	return fallback
+}
